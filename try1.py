e26a981c378d66d574745718e2fa9a6aae9b4f5e
Lewous <lmnsodv@gmail.com>
2021-01-06 23:27:05 +0800

    day job, basic create time_analysis

--- try1.py
+++ try1.py
@@ -3,21 +3,25 @@
 
 # for experimentally usage
 
+import warnings
 from pkmap import pkmap
-from pkmap_data import AD, pat_data
+from pkmap_data import AD, pat_data, app_data
 # from ekmapTK import KM
 import numpy as np
 from pandas import DataFrame
 
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 
+from tqdm import tqdm
 from sklearn.cluster import KMeans
 
-def load1():
+
+def load1(house_nuimber, interval:str='min'):
     """
     docstring
     """
-    p1 = pkmap('./REFIT/CLEAN_House6.csv', count=False)
+    p1 = pkmap('./REFIT/CLEAN_House' + str(house_nuimber) + '.csv', count=False)
     # pkmap.plot()
     # print(pkmap.data0)
 
@@ -25,32 +29,44 @@ def load1():
     from 17'51" to 18'51"
     set 0'-9" for the first
     '''
-
-    data = p1.data0
-
+    time_para = {
+        # last_sec, time_mag, time_tail, time_cut
+        # start from Time='2013-11-28 12:15:35'
+        'min':(-9, 60, ':00', -3),
+        'hr': (-9-15*60, 60*60, ':00:00', -6),
+        'day': (-9-15*60-12*60*60, 60*60*24, ' 00:00:00', -9)
+    }
+    data = p1.data0[:1800000]
+    app_para = app_data[house_nuimber]
     last_time = ""
-    # -9    ->  each minite
-    # -944  ->  each hour
-    last_sec = -9 - 15*60
+
+    if interval.lower() in ('m', 'min', 'mins', 'minite', 'minites'):
+        last_sec, time_mag, time_tail, time_cut = time_para['min']
+    elif interval.lower() in ('h', 'hr', 'hrs', 'hour', 'hours'):
+        last_sec, time_mag, time_tail, time_cut = time_para['hr']
+    elif interval.lower() in ('d', 'day', 'days'):
+        last_sec, time_mag, time_tail, time_cut = time_para['day']
+    else:
+        last_sec, time_mag, time_tail, time_cut = time_para['min']
+        Warning('\t:unrecognized interval: ' + interval + ', use "min" instead')
+    print((last_sec, time_mag, time_tail, time_cut))
     ind = 0
     cols = ('Time', 'Aggregate', 'Appliance1', 'Appliance2', 'Appliance3', 
             'Appliance4', 'Appliance5', 'Appliance6', 
             'Appliance7', 'Appliance8', 'Appliance9')
-    data2 = DataFrame(columns=cols)
+    data02 = DataFrame(columns=cols)
     apps = {col:None for col in cols}
     first = True
-    for it in data.loc[:, cols].itertuples(index=False): 
+    for it in tqdm(data.loc[:, cols].itertuples(index=False), 
+                    total=p1.len, ascii=False, leave=False): 
         # print(it)
-        # [:-3] ->  each minite
-        # [:-6] ->  each hour
-        # [:-9] ->  each day
-        # == warning: not starting from zero hour nor zero day! ==
-        if first:
-            print(it)
-            first = False
-        new_time = it.Time[:-6]
+
+        # if first:
+        #     # print(it)
+        #     first = False
+        new_time = it.Time[:time_cut]
         new_sec = int(it.Time[-2:])
-        time_tail = ':00:00'
+        
         # ind = it.Index
         sec = new_sec - last_sec
         sec = sec if sec > 0 else sec + 60
@@ -58,9 +74,22 @@ def load1():
             if last_time:
                 # sec +=  60
                 # do without first time
-                data2.loc[ind, 'Time'] = last_time + time_tail
+                data02.loc[ind, 'Time'] = last_time + time_tail
+                if first:
+                    print(apps)
+                    first = False
                 for col in cols[1:]:
-                    data2.loc[ind, col] = apps[col]
+                    # if False and apps[col] < app_para[col]['thrd'] * time_mag:
+                    if apps[col] < time_mag:
+                        data02.loc[ind, col] = None
+                    else:
+                        mean = app_para[col]['mean'] * time_mag 
+                        std = app_para[col]['std'] * time_mag
+                        if std > 0:
+                            data02.loc[ind, col] = (apps[col] / mean) 
+                            # data02.loc[ind, col] = 
+                        else:
+                            data02.loc[ind, col] = None
                 ind += 1
             # re-set
             last_time = new_time
@@ -70,49 +99,61 @@ def load1():
         else:
             # sec = new_sec - last_sec
             apps = {col:apps[col]+k*sec for col, k in zip(cols[1:],  it[1:])}
+        # if first:
+        #     print(apps)
         last_sec = new_sec
 
     '''
-    data2 is:
-                Time Aggregate Appliance1 ... Appliance4 Appliance5 ... Appliance9
+    data02 is:
+                   Time Aggregate Appliance1 ... Appliance4 Appliance5 ... Appliance9
     0  2013-11-28 12:15     15988          0 ...        420        796 ...        420
     1  2013-11-28 12:16     15887          0 ...        450        810 ...        420
     2  2013-11-28 12:17     15811          0 ...        434        811 ...        405
     ......
     '''
 
-    return data2
+    return data02
 
 
-def plot_time():
+def plot_time(house_number: int=6):
     """
     docstring
     """
 
-    # data2 = load1()
-    data2 = load1()
-    cols = tuple(data2.columns)
-    # print(p1.data0)
-    # data3 = data2.loc[:, cols[1:]].T
-    time = data2.Time
-    data2 = DataFrame(data2.loc[:, cols[1:]].T, dtype='float_')
-    print(data2)
-    data2[data2<100] = None
-    data2 = np.log(data2)
-    data2.index = cols[1:]
-    data2.columns = time
-    print(data2.index)
-    print(data2.columns)
-    # print(data2.dtypes)
+    data02 = load1(house_number, interval='hr')
+    cols = tuple(data02.columns)
+    time = data02.Time
+    data02 = DataFrame(data02.loc[:, cols[1:]].T, dtype='float_')
+    # data02[data02<1] = None
+    print(data02)
+    # data02 = np.log(data02)
+    data02.index = cols[1:]
+    data02.columns = time
+    print(data02.index)
+    print(data02.columns)
+    # print(data02.dtypes)
 
     # plotting below
     # fig = plt.figure(figsize=(12, 5))
     # ax = fig.add_axes( )
-    fig, ax = plt.subplots(1,1,figsize=(12, 5))
-    ax.pcolor(data2, cmap='inferno')
+    fig, (ax, ax2) = plt.subplots(2,1,figsize=(15, 9))
+    ax_1 = fig.add_axes([0.94, 0.5, 0.01, 0.4])
+    # ax_1.set_visible = False
+    # plot1 = ax.pcolormesh(data02, cmap='inferno', norm=cLogNorm())
+    plot1 = ax.imshow(data02, cmap='inferno', norm=LogNorm(), 
+                    aspect='auto', interpolation='none', resample=False)
     # ax.set_xticklabels(time, rotation=45)
     ax.set_yticks(range(10))
     ax.set_yticklabels(cols[1:])
+    time2 = DataFrame([(ind,k) for ind, k in time.items() if k[-8:]=='00:00:00'])
+    xrange = time2.reindex(np.linspace(0, time2.size-1, 8, dtype='int_'))[0]
+    xrange = xrange.dropna()
+    # see https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#deprecate-loc-reindex-listlike
+    print(xrange)
+    ax.set_xticks(xrange)
+    ax.set_xticklabels([time[ind][:-6] for ind in xrange])
+    ax.set_title(r'$\times$ mean of each appliance', fontsize=20)
+    fig.colorbar(plot1, cax = ax_1)
     plt.show()
 
 
@@ -123,11 +164,11 @@ def do1():
 
     len = 120
     X = np.random.rand(120, 10)
-    data2 = DataFrame(X, index=['sfsf ' + str(k) for k in range(len)], 
+    data02 = DataFrame(X, index=['sfsf ' + str(k) for k in range(len)], 
             columns=['Aggregate'] + ['Appliance' + str(k+1) for k in range(9)])
     
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8,6))
-    ax1.pcolor(data2.T, cmap='inferno')
+    ax1.pcolor(data02.T, cmap='inferno')
     print((ax1.get_xbound(), ax1.get_ybound()))
     plt.show()
 
@@ -138,7 +179,7 @@ def do2(house_number=7):
     """
 
     p1 = pkmap('./REFIT/CLEAN_House' + str(house_number) + '.csv')
-    data2 = p1.data0
+    data02 = p1.data0
     print(p1.app_name)
 
     with open('dd', 'a') as f:
@@ -150,7 +191,7 @@ def do2(house_number=7):
             'Appliance4', 'Appliance5', 'Appliance6', 
             'Appliance7', 'Appliance8', 'Appliance9')
     for ind, col in enumerate(cols):
-        a[ind] = data2[col]
+        a[ind] = data02[col]
         k[ind] = KMeans(n_clusters=2).fit(np.array(a[ind]).reshape(-1,1))
         print(f'{k[ind].cluster_centers_}')
         if np.diff(k[ind].cluster_centers_.reshape(1,-1))[0][0]>1e-5:
@@ -195,7 +236,7 @@ def do2(house_number=7):
 if __name__ == "__main__":
     
     # do1()
-    plot_time()
+    plot_time(6)
     # for ind in (0,):
     #     do2(ind+1)
 
