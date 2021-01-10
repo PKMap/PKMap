a1b21807d913cf1f12d4bc42e4b5a928105ca53c
Lewous <lmnsodv@gmail.com>
2021-01-10 19:24:09 +0800

    merge the time_preview & its data generator

--- pkmap.py
+++ pkmap.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 # === pkmap.py ===
 
-# from pandas.io.parsers import read_csv
-from ekmapTK import do_plot, read_REFIT
-from ekmapTK import do_plot
-from ekmapTK import do_plot_time
-
+import os
 from re import findall, match
+from copy import copy
 
 import matplotlib.pyplot as plt
+from numpy.lib.npyio import save
 # from pandas import DataFrame 
 from pandas import read_excel
 
-import os
-from copy import copy
+from numpy import diff, array
+from sklearn.cluster import KMeans
+
+
+from ekmapTK import do_plot, read_REFIT
+from ekmapTK import do_plot
+from ekmapTK import do_plot_time
+from house_preview import plot_time
 
 
 class pkmap(object):
@@ -75,13 +79,80 @@ class pkmap(object):
         return None
 
 
-    def plot_time(self, **args):
+    def preview(self, **args):
         """
         docstring
         """
-        do_plot_time(self, **args)
+        plot_time(self, house_number=self.house_number, 
+                  app_name=self.app_name,
+                  **args)
         # pass
 
         return None
 
 
+    def generate(self, save_name: str='mean&std_ON'):
+        """
+        generate mean and std of each app
+
+        save_name: file name to write
+                    (will be rewrited)
+
+        return: None
+        """
+        with open(save_name, 'w') as f:
+            f.write('')
+
+        with open(save_name, 'a') as f:
+            f.write('# House {}\n'.format(self.house_number))
+            f.write('{}:{\n'.format(self.house_number))
+        a = list(range(10))
+        k = list(range(10))
+        cols = ('Aggregate', 'Appliance1', 'Appliance2', 'Appliance3', 
+                'Appliance4', 'Appliance5', 'Appliance6', 
+                'Appliance7', 'Appliance8', 'Appliance9')
+        for ind, col in enumerate(cols):
+            a[ind] = self.data0[col]
+            k[ind] = KMeans(n_clusters=2).fit(array(a[ind]).reshape(-1,1))
+            print(f'{k[ind].cluster_centers_}')
+            if diff(k[ind].cluster_centers_.reshape(1,-1))[0][0]>1e-5:
+                isoff = a[ind][k[ind].labels_<1]
+                ison = a[ind][k[ind].labels_>0]
+            elif diff(k[ind].cluster_centers_.reshape(1,-1))[0][0]<-1e-5:
+                isoff = a[ind][k[ind].labels_>0]
+                ison = a[ind][k[ind].labels_<1]
+            else:
+                # cluster_centers_ may be [[0.], [0.]]
+                with open(save_name, 'a') as f:
+                    f.write('\t"{}":{'.format(col))
+                    if col != 'Aggregate':
+                        f.write('\n\t\t"name": "{}",'.format(self.app_name[ind-1]))
+                    f.write('\n\t\t"thrd": 0')
+                    f.write(',\n\t\t"mean": 0.0')
+                    f.write(',\n\t\t"std": 0.0')
+                    f.write(',\n\t},\n')
+                continue
+
+            max = isoff.max()
+            min = ison.min()
+            thrd = int((max+min)/2)
+            print(thrd)
+
+            mean = ison.mean()
+            std = ison.std()
+            with open(save_name, 'a') as f:
+                f.write('\t"' + col + '":{')
+                if col != 'Aggregate':
+                    f.write('\n\t\t"name": "{}",'.format(self.app_name[ind-1]))
+                f.write('\n\t\t"thrd": {}'.format(thrd))
+                f.write(',\n\t\t"mean": {}'.format(mean))
+                f.write(',\n\t\t"std": {}'.format(std))
+                f.write(',\n\t},\n')
+        with open(save_name, 'a') as f:
+            f.write('},\n')
+
+        return None
+
+
+if __name__ == "__main__":
+    pass
