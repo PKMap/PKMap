f6ed1b0d45485ee2e5e821622b0577533a60632a
Lewous <lmnsodv@gmail.com>
2021-01-11 21:54:19 +0800

    fix details

--- house_preview.py
+++ house_preview.py
@@ -32,7 +32,7 @@ class PointBrowser:
     and 'p' keys to browse through the next and previous points
     """
 
-    def __init__(self, ax, ax2, fig):
+    def __init__(self, ax, ax2, fig, interval):
         # self.lastind = 0
 
         # self.text = ax.text(0.05, 0.95, 'selected: none',
@@ -51,7 +51,16 @@ class PointBrowser:
                       'Appliance7', 'Appliance8', 'Appliance9')
         self.ax20 = None
 
-        self.scope = int(24)
+        if interval == 'hour':
+            self.scope = 32
+        elif interval == 'day':
+            self.scope = 2
+        elif interval == 'min':
+            self.scope = 180
+        self.noplot = True
+        self.ConPatch = []
+
+        print('\n\tclick the axis above to preview')
 
 
     def on_click(self, event):
@@ -60,12 +69,12 @@ class PointBrowser:
         """
 
         if event.inaxes == self.ax:
-            print((event.xdata, event.ydata))
-            print((self.inds[int(event.ydata)], self.cols[int(event.xdata)]))
+            # print((event.xdata, event.ydata))
+            # print((self.inds[int(event.ydata)], self.cols[int(event.xdata)]))
             
             self.ind_get = np.round(event.xdata).astype(np.uint32)
             self.y_get = np.round(event.ydata).astype(np.uint8)
-            print((self.ind_get, self.y_get))
+            # print((self.ind_get, self.y_get))
             self.time_select = self.cols[self.ind_get]
             self.app_select = self.inds[self.y_get]
             self.app_select_ = self.inds_[self.y_get]
@@ -77,6 +86,9 @@ class PointBrowser:
         """
         docstring
         """
+        if self.noplot:
+            return None
+
         key = event.key
         if key == 'up':
             if self.y_get > 0:
@@ -108,24 +120,25 @@ class PointBrowser:
             self.refresh()
 
 
-    def add_patch(self, ind_low):
+    def add_patch(self, ind_low, ind_high):
         """
         docstring
         """
-        [p.remove() for p in self.ax.patches]
-        plt.pause(1e-5)
+
         self.ax.add_patch(
             mpat.Rectangle((ind_low, self.y_get-0.5), 
                             width=2*self.scope, height=1, 
                             color='c', fill=False, lw = 1)
         )
-        # pats = [range(1)]
-        # pats[0] = mpat.Rectangle((ind_low, self.y_get-0.5), 
-        #                         width=2*self.scope, height=1, 
-        #                         color='c', fill=False, lw = 1)
-
-        # for pat in pats:
-        #     self.ax.add_patch(copy(pat))
+        
+        # add cross axes patch (line)
+        xy1 = ((ind_low, self.y_get+0.5), (ind_high, self.y_get+0.5))
+        xy2 = ((self.xlim[0], self.ax2.get_ylim()[1]), 
+               (self.xlim[1], self.ax2.get_ylim()[1]))
+        self.ConPatch = [self.fig.add_artist(mpat.ConnectionPatch(
+                                            xyA=xyA, coordsA=self.ax.transData, 
+                                            xyB=xyB, coordsB=self.ax2.transData,)
+                        ) for xyA, xyB in zip(xy1, xy2)]
 
 
     def refresh(self):
@@ -147,7 +160,8 @@ class PointBrowser:
         self.ax2.plot(data_2plot['Aggregate'], 'gray', label='Aggregate')
         self.ax2.set_xlabel('date')
         self.ax2.set_ylabel('Aggregate')
-        self.ax2.set_xlim(data_2plot.index[0], data_2plot.index[-1])
+        self.xlim = (data_2plot.index[0], data_2plot.index[-1])
+        self.ax2.set_xlim(self.xlim)
         self.ax2.set_ylim(bottom=0)
 
         """
@@ -170,9 +184,7 @@ class PointBrowser:
                     time_labels.append('')
 
         time2x = DataFrame([time_ticks, time_labels]).T
-        # time2 = DataFrame([(ind,k) for ind, k in data_2plot.Time.items() if k[11:13]=='00'])
-        print(time2x)
-
+        # print(time2x)
         self.ax2.set_xticks(time2x[0])
         self.ax2.set_xticklabels(time2x[1])
 
@@ -190,9 +202,14 @@ class PointBrowser:
                 print(self.ax2.get_ylim()[1])
                 self.ax20.set_ylim(top=self.ax2.get_ylim()[1])
         self.ax2.legend()
-        self.add_patch(ind_low)
+        
+        [p.remove() for p in self.ax.patches]
+        [p.remove() for p in self.ConPatch]
+        plt.pause(1e-5)
 
+        self.add_patch(ind_low, ind_high)
         self.fig.canvas.draw()
+        self.noplot = False
 
 
 def load1(self, house_number, interval:str='hr'):
@@ -201,80 +218,53 @@ def load1(self, house_number, interval:str='hr'):
     """
     global data02, data0
 
-    # p1 = pkmap('./REFIT/CLEAN_House' + str(house_number) + '.csv', count=False)
-    # data0 = p1.data0
-    # pkmap.plot()
-    # print(pkmap.data0)
+    time_para = {
+        # last_sec, time_mag, time_tail, time_cut, scope_default
+        # start from Time='2013-11-28 12:15:35'
+        'min':(0, 60, ':00', -3, ),
+        'hour': (-15*60, 60*60, ':00:00', -6),
+        'day': (-15*60-12*60*60, 60*60*24, ' 00:00:00', -9)
+    }
 
-    file2save = 'house'+ str(house_number) + '_in_hour.csv'
+    last_sec, time_mag, time_tail, time_cut = time_para[interval]
+    
+    data0 = self.data0
+    file2save = 'house{}_by_{}.csv'.format(house_number, interval)
     path2save = '/'.join([self.file_dir, file2save])
     for file in os.scandir(self.file_dir):
         if file2save == file.name:
             # load exist file
-            print('\t:loading old data')
+            print('\tloading old data')
             data02 = read_csv(path2save, dtype={'Time':'str'})
             # print(data02)
             # print(data02.dtypes)
             return data02
-
     else:
         print('\t:counting {}'.format(file2save))
         # data02 = load1(house_number, interval='hr')
         # data02.to_csv(path2save, index=False)
 
-    '''
-    from 17'51" to 18'51"
-    set 0'-9" for the first
-    '''
-    time_para = {
-        # last_sec, time_mag, time_tail, time_cut
-        # start from Time='2013-11-28 12:15:35'
-        'min':(-9, 60, ':00', -3),
-        'hr': (-9-15*60, 60*60, ':00:00', -6),
-        'day': (-9-15*60-12*60*60, 60*60*24, ' 00:00:00', -9)
-    }
-    data = self.data0
     app_para = app_data[house_number]
     last_time = ""
 
-    if interval.lower() in ('m', 'min', 'mins', 'minite', 'minites'):
-        last_sec, time_mag, time_tail, time_cut = time_para['min']
-    elif interval.lower() in ('h', 'hr', 'hrs', 'hour', 'hours'):
-        last_sec, time_mag, time_tail, time_cut = time_para['hr']
-    elif interval.lower() in ('d', 'day', 'days'):
-        last_sec, time_mag, time_tail, time_cut = time_para['day']
-    else:
-        last_sec, time_mag, time_tail, time_cut = time_para['min']
-        Warning('\t:unrecognized interval: ' + interval + ', use "min" instead')
-    print((last_sec, time_mag, time_tail, time_cut))
+    # print((last_sec, time_mag, time_tail, time_cut))
     ind = 0
     cols = ('Time', 'Aggregate', 'Appliance1', 'Appliance2', 'Appliance3', 
             'Appliance4', 'Appliance5', 'Appliance6', 
             'Appliance7', 'Appliance8', 'Appliance9')
     data02 = DataFrame(columns=cols)
     apps = {col:None for col in cols}
-    first = True
-    for it in tqdm(data.loc[:, cols].itertuples(index=False), 
+    for it in tqdm(data0.loc[:, cols].itertuples(index=False), 
                     total=self.len, ascii=False, leave=False): 
-        # print(it)
-
-        # if first:
-        #     # print(it)
-        #     first = False
         new_time = it.Time[:time_cut]
         new_sec = int(it.Time[-2:])
         
-        # ind = it.Index
         sec = new_sec - last_sec
         sec = sec if sec > 0 else sec + 60
         if new_time != last_time:
             if last_time:
-                # sec +=  60
                 # do without first time
                 data02.loc[ind, 'Time'] = last_time + time_tail
-                if first:
-                    print(apps)
-                    first = False
                 for col in cols[1:]:
                     # if False and apps[col] < app_para[col]['thrd'] * time_mag:
                     if apps[col] < time_mag:
@@ -294,11 +284,11 @@ def load1(self, house_number, interval:str='hr'):
             # print((last_time, apps))
 
         else:
-            # sec = new_sec - last_sec
+            # new_time == last_time
             apps = {col:apps[col]+k*sec for col, k in zip(cols[1:],  it[1:])}
-        # if first:
-        #     print(apps)
+
         last_sec = new_sec
+        # loop here
 
     '''
     data02 is:
@@ -324,6 +314,18 @@ def plot_time(self,
     """
     global data02, data0
 
+    # nomeralize `interval`
+    if interval.lower() in ('m', 'min', 'mins', 'minite', 'minites'):
+        interval = 'min'
+    elif interval.lower() in ('h', 'hr', 'hrs', 'hour', 'hours'):
+        interval = 'hour'
+    elif interval.lower() in ('d', 'day', 'days'):
+        interval = 'day'
+    else:
+        interval_default = 'day'
+        Warning('\t:unrecognized interval: {}, use "{}" instead'.format(interval, interval_default))
+        interval = interval_default
+    
     data0 = self.data0
     data02 = load1(self, house_number=house_number, interval=interval)
     cols = tuple(data02.columns[1:])
@@ -331,7 +333,6 @@ def plot_time(self,
     data02 = DataFrame(data02.loc[:, cols].T, dtype='float_')
     # data02[data02<1] = None
     # print(data02)
-    # data02 = np.log(data02)
     if app_name:
         cols = tuple(['Aggregate'] + list(app_name))
     
@@ -341,126 +342,46 @@ def plot_time(self,
     # print(data02.columns)
     # print(data02.dtypes)
 
-    # plotting below
+    """
+    do plotting below
+    """
     if noax2:
-        fig, ax = plt.subplots(1,1,figsize=(15,9))
+        fig, ax = plt.subplots(1,1,figsize=(15,6))
+        ax2 = None      # eliminate unbound warnings
     else:
         fig, (ax, ax2) = plt.subplots(2,1,figsize=(15, 9))
     ax_1 = fig.add_axes([0.94, 0.5, 0.01, 0.4])
     # ax_1.set_visible = False
     # plot1 = ax.pcolormesh(data02, cmap='inferno', norm=cLogNorm())
-    plot1 = ax.imshow(data02, cmap='inferno', norm=LogNorm(), 
+    plot1 = ax.imshow(data02, cmap='inferno_r', norm=LogNorm(), 
                     aspect='auto', interpolation='none', resample=False)
-    # ax.set_xticklabels(time, rotation=45)
     ax.set_yticks(range(10))
     ax.set_yticklabels(cols)
     time2 = DataFrame([(ind,k) for ind, k in time.items() if k[11:13]=='00'])
     # print(time2)
-    # print(DataFrame([(ind,k) for ind, k in time.items() if k[5:10]=='04-01']))
-    # print(np.linspace(0, len(time2)-1, 8, dtype='int_'))
-    # print(time2.reindex(np.linspace(0, len(time2)-1, 8, dtype='int_'))[1])
-    xrange = time2.reindex(np.linspace(0, len(time2)-1, 15, dtype='int_'))
-    # xrange = time2.copy()
-    xrange = xrange.dropna()
+    xrange = time2.reindex(np.linspace(0, len(time2)-1, 15, dtype='int_')).dropna()
+    # xrange = xrange.dropna()
     # see https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#deprecate-loc-reindex-listlike
-    print(xrange)
+    # print(xrange)
     ax.set_xticks(xrange[0])
     ax.set_xticklabels([k[:10] if np.mod(n,2) else '' for k,n in zip(xrange[1], range(1,22)) ])
-    ax.set_title(r'time analysis of House ' + str(house_number) + ' by hour', fontsize=20)
-    fig.colorbar(plot1, cax = ax_1, label=r'by mean of each appliance')
-    
+    ax.set_title('time analysis of House {} by {}'.format(house_number, interval), fontsize=20)
+    fig.colorbar(plot1, cax = ax_1, label='by mean of each appliance')
+
     if not noax2:
-        brower = PointBrowser(ax, ax2, fig)
+        brower = PointBrowser(ax, ax2, fig, interval)
         fig.canvas.mpl_connect('button_release_event', brower.on_click)
         fig.canvas.mpl_connect('key_release_event', brower.on_key)
 
     # fig.tight_layout()
+    # print(fig.set_dpi(200))
     plt.show()
 
-
-def do1():
-    """
-    docstring
-    """
-
-    len = 120
-    X = np.random.rand(120, 10)
-    data02 = DataFrame(X, index=['sfsf ' + str(k) for k in range(len)], 
-            columns=['Aggregate'] + ['Appliance' + str(k+1) for k in range(9)])
-    
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8,6))
-    ax1.pcolor(data02.T, cmap='inferno')
-    print((ax1.get_xbound(), ax1.get_ybound()))
-    plt.show()
-
-
-def do2(self, house_number=7):
-    """
-    generate mean and std of each app
-    """
-    global data0
-    # p1 = pkmap('./REFIT/CLEAN_House' + str(house_number) + '.csv')
-    data02 = data0
-    print(p1.app_name)
-
-    with open('dd', 'a') as f:
-        f.write('# House ' + str(house_number) + '\n')
-        f.write(str(house_number) + ':{\n')
-    a = list(range(10))
-    k = list(range(10))
-    cols = ('Aggregate', 'Appliance1', 'Appliance2', 'Appliance3', 
-            'Appliance4', 'Appliance5', 'Appliance6', 
-            'Appliance7', 'Appliance8', 'Appliance9')
-    for ind, col in enumerate(cols):
-        a[ind] = data02[col]
-        k[ind] = KMeans(n_clusters=2).fit(np.array(a[ind]).reshape(-1,1))
-        print(f'{k[ind].cluster_centers_}')
-        if np.diff(k[ind].cluster_centers_.reshape(1,-1))[0][0]>1e-5:
-            isoff = a[ind][k[ind].labels_<1]
-            ison = a[ind][k[ind].labels_>0]
-        elif np.diff(k[ind].cluster_centers_.reshape(1,-1))[0][0]<-1e-5:
-            isoff = a[ind][k[ind].labels_>0]
-            ison = a[ind][k[ind].labels_<1]
-        else:
-            # cluster_centers_ may be [[0.], [0.]]
-            with open('dd', 'a') as f:
-                f.write('\t"' + col + '":{')
-                if col != 'Aggregate':
-                    f.write('\n\t\t"name": "' + p1.app_name[ind-1] + '",')
-                f.write('\n\t\t"thrd": 0')
-                f.write(',\n\t\t"mean": 0.0')
-                f.write(',\n\t\t"std": 0.0')
-                f.write(',\n\t},\n')
-            continue
-
-        max = isoff.max()
-        min = ison.min()
-        thrd = int((max+min)/2)
-        print(thrd)
-
-        mean = ison.mean()
-        std = ison.std()
-        with open('dd', 'a') as f:
-            f.write('\t"' + col + '":{')
-            if col != 'Aggregate':
-                f.write('\n\t\t"name": "' + self.app_name[ind-1] + '",')
-            f.write('\n\t\t"thrd": ' + str(thrd))
-            f.write(',\n\t\t"mean": ' + str(mean))
-            f.write(',\n\t\t"std": ' + str(std))
-            f.write(',\n\t},\n')
-    with open('dd', 'a') as f:
-        f.write('},\n')
-
-    return None
+    # end of plot_time()
 
 
 if __name__ == "__main__":
     
-    # do1()
     # plot_time(house_number=2, )
-    # for ind in (0,):
-    #     do2(ind+1)
-    # p1 = pkmap()
-    # p1.preview()
-    pass
 
+    pass
