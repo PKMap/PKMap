223de708f1c631d98accde8161fa9ea2568d8b41
Lewous <lmnsodv@gmail.com>
2021-04-13 01:41:02 +0800

    support markdown file and add a description

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ from setuptools import setup, find_packages
 __version__ = '0.2.1'
 
 THENAME = "pkmap"
-DESCRIPTION = "xxx"
+DESCRIPTION = "a dataset visualization generator supposed to \
+               exhibit the imbalance for human eye acquisition"
 MAINTAINER = "ZJ Lewous"
 MAINTAINER_EMAIL = "zj.lewous@gmail.com"
 URL = "https://github.com/pkmap/pkmap"
@@ -38,6 +39,7 @@ setup(
     url = URL,
     download_url = DOWNLOAD_URL,
     long_description = open("README.md", encoding="utf-8").read(),
+    long_description_content_type = "text/markdown",
     # long_description = "see in " + URL, 
     license = LICENSE,
 )
